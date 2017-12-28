917a6c16ebc5e12e981ff487f1dcb90ec1233e7b
alexkandres <alexkandres@live.com>
2017-12-28 09:58:21 -0500

    no passed in steps

--- src/org/demo/buildUtils2.groovy
+++ src/org/demo/buildUtils2.groovy
@@ -3,14 +3,13 @@ package org.demo
 class buildUtils2 implements Serializable{
     def steps
 
-    buildUtils2(steps){
-        this.steps = steps
+    buildUtils2(){
     }
 
     def timedGradleBuild(tasks){
 
-        steps.timestamps{
-            steps.sh "echo 'steps passed as an object'"
+        timestamps{
+            sh "echo 'steps passed as an object'"
         }
     }
 }
