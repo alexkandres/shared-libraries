9841dbc2b4cc746dd6eae3c4e6658992f00bb9da
alexkandres <alexkandres@live.com>
2017-12-29 08:32:06 -0500

    Example 3

--- src/org/demo/buildUtils3.groovy
+++ src/org/demo/buildUtils3.groovy
@@ -3,7 +3,7 @@ package org.demo
 class buildUtils3 implements Serializable{
 
     //use steps object to execute pipeline commands
-    def timedGradleBuild(script, tasks){
+    static def timedGradleBuild(script, tasks){
         script.sh "echo 'Environment NAME = ${script.env.NAME}'"
         script.timestamps{
             script.sh "echo 'execute gradle ${tasks}'"
